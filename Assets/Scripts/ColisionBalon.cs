5fd2cee6bba6e7027d8d9b726fa994ba54340461
libardo2s <libardoii@hotmail.com>
2015-09-28 20:45:07 -0500

    The robot move the ball

---
+++ Assets/Scripts/ColisionBalon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColisionBalon : MonoBehaviour {
+	//private Rigidbody rb;
+	private float force;
+	
+	void Start(){
+		///rb = GetComponent<Rigidbody>();
+		force = 0;
+	}
+	
+	void Update(){
+		transform.position = transform.position + Vector3.back * force;
+	}
+	
+	void OnTriggerEnter(Collider other) {
+            //set the velocity, so you can move
+			 force = 1f;
+			 
+    }
+	
+	void OnTriggerExit(Collider other) {
+            //set the velocity, so you can move
+			 force = 0;
+    }
+}
---
+++ Assets/Scripts/MasCercano.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasCercano : MonoBehaviour {
+
+	// Use this for initialization
+	public NavMeshAgent[] playerList;
+	private Transform ballPosition;
+	private float[] distancias;
+	void Start () {
+		GameObject ball = GameObject.FindWithTag("Ball");
+		ballPosition = ball.transform;
+		distancias = new float[playerList.Length];
+	}
+	void Update () {
+		CalculateDistance();		
+	}
+	
+	void CalculateDistance(){
+		for(int i=0;i<playerList.Length;i++){
+			distancias[i] = Vector3.Distance(playerList[i].gameObject.transform.position,ballPosition.position);
+		}
+		
+		float minValue = distancias[0];
+		int indice = 0;
+		
+		for (int i = 0; i < distancias.Length; i++)
+		{
+			if (distancias[i] < minValue){
+				minValue = distancias[i];
+				indice = i;				
+			}
+		}
+		
+		playerList[indice].SetDestination(ballPosition.position);
+	}
+}
